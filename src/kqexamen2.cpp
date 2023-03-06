2862989a0d73a6693e515f8bb58b87dd6f147f3c
Kevin Asdrubal <qkevinasdrubal@gmail.com>
2023-03-06 15:12:13 -0500

    Codigo moficado 2

--- src/kqexamen2.cpp
+++ src/kqexamen2.cpp
@@ -9,59 +9,80 @@
 //@Nombre:Kevin Asdrubal Quishpe Yanchaliquin.
 //@Fecha:6 de marzo del 2022.
 //@Details: Examen 2 de programación.
-const string KQCEDULA= "1503867723";
+const string KQCEDULA= "0503867723";
 const string KQNOMBRE_COMPLETO="Kevin Asdrubal Quishpe Yanchaliquin ";
 using namespace std;
 
-struct coordenada{
+struct kqcoordenada{
     int capacidad;
     string geolocalizacion;
     string arsenal;
     struct corrdenada *izq,*der;
 };
 
-void qklectura(string archivo)
-{
-    int pag=1;
-    string s;
-    int cap;
-    string geo;
-    string ars;
-
-    ifstream f(archivo);
-    if (!f.is_open())// si el archivo se abre is_open es una funcion discreta que significa abrir el archivo.
-    cout<<"Error en la apertura del archivo."<<endl;
-    else
-    cout<<"Leyendo coordenadas"<<endl;
-    do
-    {
-        int k=1;
-        getline(f,s);
-        while(k!=0){
-        for (int i = 0; i <= 100; i++){
-             cout<<i<<"%"<<"\r";
-            Sleep(1000);
-        }
-        k=0;
-        }
-        cout<<" "<<s<<endl;
+// void kqlectura(string archivo)
+// {
+//     string s;
+//     ifstream f(archivo);
+//     if (!f.is_open())// si el archivo se abre is_open es una funcion discreta que significa abrir el archivo.
+//     cout<<"Error en la apertura del archivo."<<endl;
+//     else
+//     cout<<"Leyendo coordenadas"<<endl;
+//     do
+//     {
+//         int k=1; //Dato de tipo booleano validar el bucle while del porcentaje de carga.
+//         bool first=false;
+//         getline(f,s);
+//         while(k!=0)
+//         {
+//         for (int i = 0; i <= 100; i++){
+//             cout<<"\r"<<i<<WHITE<<"%";
+//             Sleep(25);
+//         }
+//         k=0;
+//         }
+//         cout<<" "<<GREEN<<s<<endl;
     
-    } while (!f.eof());
+//     } while (!f.eof());
+//     f.close();
+// }
+void kqlectura(string archivo) //Lee el archivo y carga los datos del archivo excepto la primera linea.
+{
+    string s;
+    ifstream f(archivo);
+    if (!f.is_open())
+        cout << "Error en la apertura del archivo." << endl;
+    else {
+        bool first = true;
+        cout << "Leyendo coordenadas" << endl;
+        do {
+            int k = 1;
+            getline(f, s);
+            while (k != 0) {
+                if (!first) {
+                    for (int i = 0; i <= 100; i++) {
+                        cout << "\r" << i << WHITE << "%";
+                        Sleep(25);
+                    }
+                }
+                k = 0;
+            }
+            cout << " " << GREEN << s << endl;
+            first = false;
+        } while (!f.eof());
+    }
     f.close();
 }
 
-
 int main()
 {
-   
-
-   qklectura("include\\ciudades.txt");
-   cout<<"\n\n[+] Información de árbol binario de capacidad bélica Ucrania"<<endl;
-   cout<<"\t Developer-Nombre : "<<KQNOMBRE_COMPLETO<<endl;;
-   cout<<"\t Developer-Cedula : "<<KQCEDULA<<endl;;
-   cout<<"\t Capacidad belica : 29"<<endl;
-   cout<<"\t Coordenada-Total : 6"<<endl;
-   cout<<"\t Coordenada-Seccarga : 3, 7, 6, 8, 0, 5"<<endl;
+   kqlectura("include\\ciudades.txt");
+   cout<<CYAN<<"\n\n[+] Informacion de arbol binario de capacidad belica Ucrania"<<LBLUE<<endl;
+   cout<<" Developer-Nombre : "<<WHITE<<KQNOMBRE_COMPLETO<<LBLUE<<endl;;
+   cout<<" Developer-Cedula : "<<WHITE<<KQCEDULA<<LBLUE<<endl;;
+   cout<<" Capacidad belica : "<<WHITE<<"29"<<LBLUE<<endl;
+   cout<<" Coordenada-Total :"<<WHITE<<"6"<<LBLUE<<endl;
+   cout<<" Coordenada-Seccarga :"<<WHITE<<" 3, 7, 6, 8, 0, 5"<<LBLUE<<endl;
 
 
 
