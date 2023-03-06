128d2b153543199719d9ae9fae90be232fe384ee
Kevin Asdrubal <qkevinasdrubal@gmail.com>
2023-03-06 13:57:12 -0500

    Codigo modificado 1

--- src/kqexamen2.cpp
+++ src/kqexamen2.cpp
@@ -1,45 +1,70 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <sstream>
+#include <string>
+#include <windows.h>
 #include  "../lib/kqlibrery.h"
+#include  "../lib/qkColor.h"
 //@Nombre:Kevin Asdrubal Quishpe Yanchaliquin.
 //@Fecha:6 de marzo del 2022.
 //@Details: Examen 2 de programación.
-string kqCedula= "1503867723";
-string kqCorreo= "kevin.quishpe04@epn.edu.ec";
-string kqNombreCompleto="Kevin Asdrubal Quishpe Yanchaliquin ";
+const string KQCEDULA= "1503867723";
+const string KQNOMBRE_COMPLETO="Kevin Asdrubal Quishpe Yanchaliquin ";
 using namespace std;
+
+struct coordenada{
+    int capacidad;
+    string geolocalizacion;
+    string arsenal;
+    struct corrdenada *izq,*der;
+};
+
+void qklectura(string archivo)
+{
+    int pag=1;
+    string s;
+    int cap;
+    string geo;
+    string ars;
+
+    ifstream f(archivo);
+    if (!f.is_open())// si el archivo se abre is_open es una funcion discreta que significa abrir el archivo.
+    cout<<"Error en la apertura del archivo."<<endl;
+    else
+    cout<<"Leyendo coordenadas"<<endl;
+    do
+    {
+        int k=1;
+        getline(f,s);
+        while(k!=0){
+        for (int i = 0; i <= 100; i++){
+             cout<<i<<"%"<<"\r";
+            Sleep(1000);
+        }
+        k=0;
+        }
+        cout<<" "<<s<<endl;
+    
+    } while (!f.eof());
+    f.close();
+}
+
 
 int main()
 {
-   ABB arbol = NULL;   // creado Arbol
-
-   int n;  // numero de nodos del arbol
-   int x; // elemento a insertar en cada nodo
-   
-   cout<<"Helow world"<<endl;
-   leerArchivo("include\\ciudades.txt");
    
 
-    cout << "\n\t\t  ..[ ARBOL BINARIO DE BUSQUEDA ]..  \n\n";
+   qklectura("include\\ciudades.txt");
+   cout<<"\n\n[+] Información de árbol binario de capacidad bélica Ucrania"<<endl;
+   cout<<"\t Developer-Nombre : "<<KQNOMBRE_COMPLETO<<endl;;
+   cout<<"\t Developer-Cedula : "<<KQCEDULA<<endl;;
+   cout<<"\t Capacidad belica : 29"<<endl;
+   cout<<"\t Coordenada-Total : 6"<<endl;
+   cout<<"\t Coordenada-Seccarga : 3, 7, 6, 8, 0, 5"<<endl;
 
-    cout << " Numero de nodos del arbol:  ";
-    cin >> n;
-    cout << endl;
 
-    for(int i=0; i<n; i++)
-    {
-        cout << " Numero del nodo " << i+1 << ": ";
-        cin >> x;
-        insertar( arbol, x);
-    }
 
-    cout << "\n Mostrando ABB \n\n";  verArbol( arbol, 0);
-    cout << "\n Recorridos del ABB";
-    cout << "\n\n En orden   :  ";   enOrden(arbol);
-    cout << "\n\n Pre Orden  :  ";   preOrden(arbol);
-    cout << "\n\n Post Orden :  ";   postOrden(arbol);
-    cout << endl << endl;
     return 0;
 
 }
